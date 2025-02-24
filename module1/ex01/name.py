9fdc765e8eff023a089c7b4e1dc341771b4d0e41
umeneses <umeneses@student.42sp.org.br>
2025-02-24 12:35:36 -0300

    add module1 studies

---
+++ module1/ex01/name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.8
+
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    name.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/02/24 11:59:11 by umeneses          #+#    #+#              #
+#    Updated: 2025/02/24 11:59:12 by umeneses         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+first_name = 'Bira'
+last_name = 'Lavor'
+whitespace = ' '
+
+whole_name = first_name + whitespace + last_name
+
+print(whole_name)
---
+++ module1/ex03/whatsyourname.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.8
+
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    whatsyourname.py                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/02/24 12:29:13 by umeneses          #+#    #+#              #
+#    Updated: 2025/02/24 12:29:14 by umeneses         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+users_first_name = input('Hey, what\'s your first name? : ')
+users_last_name = input('Hey, what\'s your last name? : ')
+greeting = 'Well, pleased to meet you,'
+
+print(f'{greeting} {users_first_name.strip()} {users_last_name}.')
