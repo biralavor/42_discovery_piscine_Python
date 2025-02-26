6eb46c1c2535398926f4905bc6dbe4f2efb031bb
umeneses <umeneses@student.42sp.org.br>
2025-02-26 15:59:07 -0300

    refactor enable empty slot

--- module3/ex02/i_got_that.py
+++ module3/ex02/i_got_that.py
@@ -12,20 +12,37 @@
 #                                                                              #
 # **************************************************************************** #
 
-saved_notes = []
-users_note = input('What you gotta say? : ')
-saved_notes.append(users_note)
-while True :
-  users_note = input('I got that! Anything else? : ')
-  if users_note == 'STOP' :
-    break
-  saved_notes.append(users_note)
-if len(saved_notes) == 0 :
-  print('No notes saved.')
-else :
+def replace_if_empty(saved_notes) :
+  for idx in range(0, len(saved_notes)) :
+    if saved_notes[idx] == '' :
+      saved_notes.remove(saved_notes[idx])
+      saved_notes.insert(idx, ' :: empty slot ::')
+  return saved_notes
+
+def saved_notes_output(saved_notes) :
   print('\n>>>>>>>>>>>>>>>>>>>>>>>>>')
   print('Here is your saved notes:')
   for idx in range(0, len(saved_notes)) :
     print(f'{idx}. {saved_notes[idx]}')
     idx += 1
+
+def i_got_that() :
+  while True :
+    if saved_notes[0] == 'STOP' :
+      print('No notes saved.')
+      exit()
+    else :
+      users_note = input('I got that! Anything else? : ')
+      if users_note == 'STOP' :
+        break
+    saved_notes.append(users_note)
+  return saved_notes
+
+if __name__ == "__main__" :
+  saved_notes = []
+  users_note = input('What you gotta say? : ')
+  saved_notes.append(users_note)
+  saved_notes = i_got_that()
+  saved_notes = replace_if_empty(saved_notes)
+  saved_notes_output(saved_notes)
 exit()
