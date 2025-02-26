3c0d79930e062a68a70695ecbc79c156d399b72b
umeneses <umeneses@student.42sp.org.br>
2025-02-26 16:31:37 -0300

    add input validation to avoid bad behavior

--- module4/ex01/age.py
+++ module4/ex01/age.py
@@ -13,12 +13,25 @@
 # **************************************************************************** #
 
 def milestone_calculator(users_age, milestone) :
+  if users_age < 0 :
+    print("You are not born yet.")
+    users_age = input_validation()
   for loop in range(1, 4):
     next_age = users_age + (milestone * loop)
     next_mile = milestone * loop
     print(f'In {next_mile} years you will be {next_age} years old.')
 
+def input_validation() :
+  while True :
+    users_age = input('Please tell me your age: ')
+    try :
+      users_age = int(users_age)
+      break
+    except ValueError :
+      print("Your input wasn't a number. Please, try again!")
+  return users_age
+
 if __name__ == "__main__" :
-  users_age = int(input('Please tell me your age: '))
+  users_age = input_validation()
   milestone = 10
   milestone_calculator(users_age, milestone)
