fe94a5d7b8cb7a16be7d1946813e6d5ab2c6eaa3
biralavor <biralavor@gmail.com>
2025-03-18 15:00:28 -0300

    enable interactive mode

--- module4_Simple_Methods/ex01/age.py
+++ module4_Simple_Methods/ex01/age.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3.12
 
 # **************************************************************************** #
 #                                                                              #
@@ -12,26 +12,34 @@
 #                                                                              #
 # **************************************************************************** #
 
-def milestone_calculator(users_age, milestone) :
-  if users_age < 0 :
-    print("You are not born yet.")
-    users_age = input_validation()
-  for loop in range(1, 4):
-    next_age = users_age + (milestone * loop)
-    next_mile = milestone * loop
-    print(f'In {next_mile} years you will be {next_age} years old.')
+import sys
 
-def input_validation() :
-  while True :
-    users_age = input('Please tell me your age: ')
-    try :
-      users_age = int(users_age)
-      break
-    except ValueError :
-      print("Your input wasn't a number. Please, try again!")
-  return users_age
+class milestoneCalculator:
+  def __init__(self, age, milestone):
+    self.age = age
+    self.milestone = milestone
 
-if __name__ == "__main__" :
-  users_age = input_validation()
+  def input_validation(self):
+    while True:
+      nbr = input('Please, tell me your age: ')
+      try:
+        users_age = int(nbr)
+        break
+      except ValueError:
+        sys.stderr.write(f"'{nbr}' wasn't a number. Try again!\n")
+    return users_age
+  
+  def calculator_runner(self, users_age, milestone):
+    if users_age < 0:
+      sys.stderr.write("It seems you're NOT born yet. O.o\n")
+      users_age = self.input_validation()
+    for loop in range(1, 4):
+      next_age = users_age + (milestone * loop)
+      next_mile = milestone * loop
+      print(f'In {next_mile} years, you will be {next_age} years old.')
+
+if __name__ == "__main__":
   milestone = 10
-  milestone_calculator(users_age, milestone)
+  arg_input = milestoneCalculator(0, 0)
+  users_age = arg_input.input_validation()
+  arg_input.calculator_runner(users_age, milestone)
